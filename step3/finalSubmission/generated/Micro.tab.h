9ff50a0a5b5b956f0c83db76b45ecad5c29c7b29
Lalit Sudarshan <lsudarsh@ecegrid-thin4.ecn.purdue.edu>
2015-10-12 20:11:42 -0400

    step4 init

---
+++ step3/finalSubmission/generated/Micro.tab.h
@@ -0,0 +1,101 @@
+
+/* A Bison parser, made by GNU Bison 2.4.1.  */
+
+/* Skeleton interface for Bison's Yacc-like parsers in C
+   
+      Copyright (C) 1984, 1989, 1990, 2000, 2001, 2002, 2003, 2004, 2005, 2006
+   Free Software Foundation, Inc.
+   
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+   
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+   
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
+
+/* As a special exception, you may create a larger work that contains
+   part or all of the Bison parser skeleton and distribute that work
+   under terms of your choice, so long as that work isn't itself a
+   parser generator using the skeleton or a modified version thereof
+   as a parser skeleton.  Alternatively, if you modify or redistribute
+   the parser skeleton itself, you may (at your option) remove this
+   special exception, which will cause the skeleton and the resulting
+   Bison output files to be licensed under the GNU General Public
+   License without this special exception.
+   
+   This special exception was added by the Free Software Foundation in
+   version 2.2 of Bison.  */
+
+
+/* Tokens.  */
+#ifndef YYTOKENTYPE
+# define YYTOKENTYPE
+   /* Put the tokens into the symbol table, so that GDB and other debuggers
+      know about them.  */
+   enum yytokentype {
+     PROGRAM = 258,
+     BEGIN_TOKEN = 259,
+     END = 260,
+     FUNCTION = 261,
+     READ = 262,
+     WRITE = 263,
+     IF = 264,
+     ELSE = 265,
+     FI = 266,
+     FOR = 267,
+     ROF = 268,
+     CONTINUE = 269,
+     BREAK = 270,
+     RETURN = 271,
+     INT = 272,
+     VOID = 273,
+     STRING = 274,
+     FLOAT = 275,
+     ADDOP = 276,
+     MULOP = 277,
+     COMPOP = 278,
+     ASSMTOP = 279,
+     OPENPAROP = 280,
+     CLOSEPAROP = 281,
+     SCOLONOP = 282,
+     COMMAOP = 283,
+     INTLIT = 284,
+     FLTLIT = 285,
+     STRLIT = 286,
+     IDENTIFIER = 287
+   };
+#endif
+
+
+
+#if ! defined YYSTYPE && ! defined YYSTYPE_IS_DECLARED
+typedef union YYSTYPE
+{
+
+/* Line 1676 of yacc.c  */
+#line 30 "src/Micro.y"
+
+	int ival;
+	float fval;
+	char *sval;
+	char *iden;
+
+
+
+/* Line 1676 of yacc.c  */
+#line 93 "Micro.tab.h"
+} YYSTYPE;
+# define YYSTYPE_IS_TRIVIAL 1
+# define yystype YYSTYPE /* obsolescent; will be withdrawn */
+# define YYSTYPE_IS_DECLARED 1
+#endif
+
+extern YYSTYPE yylval;
+
+
